c646d9131629cd6e354c7b1d809158680e2abe99
antoniocirilo <antoniocirilo526@gmail.com>
2021-03-22 10:18:43 -0300

    adicionando estático

--- Estivas/Estivas/urls.py
+++ Estivas/Estivas/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ramais.views import listaramais
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', listaramais),
 ]

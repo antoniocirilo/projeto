206f45079a6bab53db81c558f18053fd4f8546c1
antoniocirilo <antoniocirilo526@gmail.com>
2021-03-26 09:19:45 -0300

    ajustando bd

--- Estivas/ramais/models.py
+++ Estivas/ramais/models.py
@@ -5,7 +5,7 @@ class Pessoa(models.Model):
 	nome = models.CharField('Nome', max_length=150)
 	setor = models.CharField('Setor', max_length=100)
 	cargo = models.CharField('Cargo', max_length=50)
-	matricula = models.CharField('Matricula', max_length=10)
+	matricula = models.CharField('Matricula', max_length=6, unique=True)
 	cc = models.CharField('Centro de custo', max_length=7, null=True)
 	ramal = models.CharField('Ramal', max_length=4)
 	foto = models.ImageField('Foto', upload_to='usuarios', default='usuarios/sem-foto.png')

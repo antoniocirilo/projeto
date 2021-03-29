b494a301099474049dd5978f3a68e32eaab0b5e6
antoniocirilo <antoniocirilo526@gmail.com>
2021-03-29 08:02:06 -0300

    ajustando email

--- Estivas/ramais/models.py
+++ Estivas/ramais/models.py
@@ -6,7 +6,7 @@ class Pessoa(models.Model):
 	setor = models.CharField('Setor', max_length=100)
 	cargo = models.CharField('Cargo', max_length=50)
 	matricula = models.CharField('Matricula', max_length=6, unique=True)
-	email = models.EmailField('Email', null=True)
+	email = models.EmailField('Email', null=True, max_length=50, unique=True)
 	cc = models.CharField('Centro de custo', max_length=7, null=True)
 	ramal = models.CharField('Ramal', max_length=4)
 	foto = models.ImageField('Foto', upload_to='usuarios', default='usuarios/sem-foto.png')
